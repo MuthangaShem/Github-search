366bcf88b7fc66dc7fe6f74c2e17e8f2446ce779
muthangashem@gmail.com <MuthangaShem@gmail.com>
2018-07-21 20:31:04 +0300

    Auto-generated commit

--- src/app/classes/user.ts
+++
@@ -1,7 +0,0 @@
-export class User {
-	constructor(private username:string, private client_id:string,private client_secret:string){
-		// username = 'muthangashem';
-		// client_id = '6228816eae502586b23a';
-		// client_secret = '02d3234aeae94509c9b75c42d35813c59e53dc9e';
-	}
-}
